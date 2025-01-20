12ab055aceb2055a2dde71d0395aa7a1735bda51
Godwin-Abin <94969757+Godwin-Abin@users.noreply.github.com>
2025-01-20 21:42:38 +0530

    Initial Commit
    Need to fix imports

---
+++ src/components/Home.js
@@ -0,0 +1,215 @@
+import React from "react";
+import "./Home.css";
+
+const demoImage = "https://via.placeholder.com/150";
+
+export const Home = () => {
+  return (
+    <div className="frame">
+      <div className="overlap">
+        <img className="yukthi-logo" alt="Yukthi logo" src={demoImage} />
+
+        <img className="blue" alt="Blue" src={demoImage} />
+
+        <img className="yasya-logo" alt="Yasya logo" src={demoImage} />
+
+        <div className="yin-yang">
+          <div className="overlap-group">
+            <div className="white-dot" />
+
+            <img className="black-dot" alt="Black dot" src={demoImage} />
+
+            <img className="black" alt="Black" src={demoImage} />
+          </div>
+        </div>
+
+        <div className="SUBJECY">
+          <div className="div">
+            <img
+              className="yasya-rbg-halftone"
+              alt="Yasya rbg halftone"
+              src={demoImage}
+            />
+
+            <img
+              className="yasys-woman-ori"
+              alt="Yasys woman ori"
+              src={demoImage}
+            />
+
+            <img
+              className="yasya-bw-halftone"
+              alt="Yasya bw halftone"
+              src={demoImage}
+            />
+          </div>
+        </div>
+
+        <img className="img" alt="Frame" src={demoImage} />
+
+        <div className="yukthi-robot">
+          <div className="overlap-2">
+            <img
+              className="yukthi-rgb-halfltone"
+              alt="Yukthi rgb halfltone"
+              src={demoImage}
+            />
+
+            <img className="vector" alt="Vector" src={demoImage} />
+
+            <img
+              className="yukthi-robot-2"
+              alt="Yukthi robot"
+              src={demoImage}
+            />
+
+            <img
+              className="yukthi-bw-halfton"
+              alt="Yukthi bw halfton"
+              src={demoImage}
+            />
+          </div>
+        </div>
+
+        <div className="elements-RIGHT">
+          <div className="group">
+            <div className="ellipse" />
+
+            <div className="ellipse-2" />
+
+            <div className="ellipse-3" />
+          </div>
+
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group-2">
+              <img className="line" alt="Line" src={demoImage} />
+
+              <img className="line-2" alt="Line" src={demoImage} />
+            </div>
+          </div>
+
+          <div className="overlap-wrapper">
+            <div className="overlap-group-2">
+              <img className="line" alt="Line" src={demoImage} />
+
+              <img className="line-2" alt="Line" src={demoImage} />
+            </div>
+          </div>
+
+          <div className="div-wrapper">
+            <div className="overlap-group-2">
+              <img className="line" alt="Line" src={demoImage} />
+
+              <img className="line-2" alt="Line" src={demoImage} />
+            </div>
+          </div>
+
+          <div className="group-2">
+            <div className="overlap-group-2">
+              <img className="line" alt="Line" src={demoImage} />
+
+              <img className="line-2" alt="Line" src={demoImage} />
+            </div>
+          </div>
+        </div>
+
+        <div className="elements-LEFT">
+          <div className="group">
+            <div className="ellipse-4" />
+
+            <div className="ellipse-5" />
+
+            <div className="ellipse-6" />
+          </div>
+
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group-2">
+              <img className="line" alt="Line" src={demoImage} />
+
+              <img className="line-2" alt="Line" src={demoImage} />
+            </div>
+          </div>
+
+          <div className="overlap-wrapper">
+            <div className="overlap-group-2">
+              <img className="line" alt="Line" src={demoImage} />
+
+              <img className="line-2" alt="Line" src={demoImage} />
+            </div>
+          </div>
+
+          <div className="div-wrapper">
+            <div className="overlap-group-2">
+              <img className="line" alt="Line" src={demoImage} />
+
+              <img className="line-2" alt="Line" src={demoImage} />
+            </div>
+          </div>
+
+          <div className="group-2">
+            <div className="overlap-group-2">
+              <img className="line" alt="Line" src={demoImage} />
+
+              <img className="line-2" alt="Line" src={demoImage} />
+            </div>
+          </div>
+        </div>
+
+        <div className="text-wrapper">YASYA ‘25</div>
+
+        <div className="text-wrapper-2">YUKTHI ‘25</div>
+
+        <div className="college-name">
+          <p className="ST-THOMAS-INSTITUTE">
+            ST. THOMAS INSTITUTE FOR SCIENCE &amp; TECHNOLoGY
+          </p>
+
+          <div className="text-wrapper-3">PRESENTS</div>
+        </div>
+
+        <div className="elements-RIGHT-2">
+          <div className="group-3">
+            <div className="ellipse-7" />
+
+            <div className="ellipse-8" />
+
+            <div className="ellipse-9" />
+          </div>
+
+          <div className="group-4">
+            <div className="overlap-group-3">
+              <img className="line-3" alt="Line" src={demoImage} />
+
+              <img className="line-4" alt="Line" src={demoImage} />
+            </div>
+          </div>
+
+          <div className="group-5">
+            <div className="overlap-group-3">
+              <img className="line-3" alt="Line" src={demoImage} />
+
+              <img className="line-4" alt="Line" src={demoImage} />
+            </div>
+          </div>
+
+          <div className="group-6">
+            <div className="overlap-group-3">
+              <img className="line-3" alt="Line" src={demoImage} />
+
+              <img className="line-4" alt="Line" src={demoImage} />
+            </div>
+          </div>
+
+          <div className="group-7">
+            <div className="overlap-group-3">
+              <img className="line-3" alt="Line" src={demoImage} />
+
+              <img className="line-4" alt="Line" src={demoImage} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
